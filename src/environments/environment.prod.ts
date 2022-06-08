47971af769d56c02f2f55efec18554cdc090100a
Juan Pablo Martinez <juan.martinez8@walmart.com>
2022-06-08 18:45:53 -0400

    fix api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true,
+  authApi: "https://precos-backend.herokuapp.com/auth/",
+  usersApi:"https://precos-backend.herokuapp.com/api/",
+  patientsApi:"https://precos-backend.herokuapp.com/api/",
+  ccrPatientsApi:"https://precos-backend.herokuapp.com/api/",
+  ccrSchedulingApi:"https://precos-backend.herokuapp.com/api/",
+  cbpPatientsApi:"https://precos-backend.herokuapp.com/api/",
+  administrativeApi:"https://precos-backend.herokuapp.com/api/"
 };
